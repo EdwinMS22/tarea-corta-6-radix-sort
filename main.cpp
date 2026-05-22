4ae448a2c4d380b396d8cf9fa3acdf409ee4c0b5
Edwin Muñoz <edwinandrey2206@gmail.com>
2026-05-21 22:53:32 -0600

    Implement positive integer input validation

--- main.cpp
+++ main.cpp
@@ -4,7 +4,34 @@
 // ############################################################
 
 #include <iostream>
+#include <string>
+#include <sstream>
+
+using std::cout;
+using std::string;
+
+static int readPositiveInt(const string& prompt, int min=1) {
+    while (true) {
+        cout << prompt;
+        string line;
+        std::getline(std::cin, line);
+
+        std::istringstream iss(line);
+        int value{};
+        if ((iss >> value) && (iss >> std::ws).eof() && value >= min)
+            return value;
+        cout << "Please enter an integer greater than or equal to " << min << ". Try again.\n";
+    }
+}
 
 int main() {
-    std::cout << "Hello World!\n";
+    int option = -1;
+    while (option != 0) {
+        cout << "\tRadix Sort\n\n";
+        int size = readPositiveInt("Enter the size of the list to sort: ");
+
+        option = 0;
+    }
+
+    return 0;
 }
